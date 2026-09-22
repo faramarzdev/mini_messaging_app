4727c569f9b8c47b9a4e86e1edda4046456bf496
FaramarzDev <69290811+faramarzdev@users.noreply.github.com>
2026-09-22 15:33:49 +0000

    fix(api): update resource classes and chat test
    - unify the ProfileResource and ChatItemResource classes to return consistent data structures using ProfileResource in ChatItemResource
    - update the ChatTest to reflect the changes in the resource classes (now contains profile picture in the test)

--- api/app/Http/Resources/ProfileResource.php
+++ api/app/Http/Resources/ProfileResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Enums\ProfileableTypes;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,11 +16,16 @@ class ProfileResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-
+            'name' => $this->profileable_type === ProfileableTypes::Channel->value
+                ? $this->name
+                : $this->profileable->name,
             'handle' => $this->handle,
+
             'featured_picture' => $this->whenLoaded('featuredPicture'),
             'pictures' => $this->whenLoaded('pictures'),
+
             'profileable_type' => $this->profileable_type,
+
             'profileable' => $this->whenLoaded('profileable', function () {
                 return $this->resolveProfileableResource($this->profileable);
             }),
